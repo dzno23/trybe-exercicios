772dac775dbfdb54b28ab0b985e8aaed6ca2a9cf
thiagohcc <dzno23@gmail.com>
2022-08-23 14:00:20 -0300

    Exercicios 4.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javacript-logica-de-programacao-e-algoritimos/exercicio07.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javacript-logica-de-programacao-e-algoritimos/exercicio07.js
@@ -1,3 +1,14 @@
+// 3- Agora inverta o lado do triângulo. Por exemplo:
+// Copiar
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+
 let n = 5;
 let a = '';
 let contador = n;
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javacript-logica-de-programacao-e-algoritimos/exercicio08.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javacript-logica-de-programacao-e-algoritimos/exercicio08.js
@@ -1,3 +1,12 @@
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// Copiar
+// n = 5
+
+//   *
+//  ***
+// *****
+
+
 let n = 5;
 let a = '';
 let contador = n;
