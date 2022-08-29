223b4598dc056d3d64610ff3255b3fc951690038
thiagohcc <dzno23@gmail.com>
2022-08-29 19:50:24 -0300

    finalizando as atividades

--- fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-03-eventos/atividades-para-fixar/main.js
+++ fundamentos/bloco-05-javascript-DOM-eventos-e-web-storage/dia-03-eventos/atividades-para-fixar/main.js
@@ -36,14 +36,36 @@ function addClasstech(objeto) {
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('keyup', editTextTech);
 
+function editTextTech() {
+    let boxTech = document.querySelector('.tech');
+    boxTech.innerText = input.value;
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener('dblclick', myWebPage);
+function myWebPage() {
+    window.open('http://thiagohcc.github.io');
+    // window.location.href = 'http://thiagohcc.github.io';  <- redireciona a mesma janela para o link.
+}
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', editColor);
+function editColor() {
+    myWebpage.style.color = '#c7a0a0';
+}
+
+myWebpage.addEventListener('mouseleave', resetColor);
+function resetColor() {
+    
+    myWebpage.style.color = '';
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
