11bdd0033d92ecccc1acd1e41e6dc7c59c4f957f
thiagohcc <dzno23@gmail.com>
2022-09-22 16:11:07 -0300

    Para fixar do dia 8.1

---
+++ fundamentos/secao-08-higher-order-functions/dia-01-introducao-a-higher-order-functions/atividades/para-fixar.js
@@ -0,0 +1,7 @@
+const wakingUp = () => 'Acordando!!';
+const breakfast = () => 'Bora tomar café!!';
+const goToSleep = () => 'Partiu dormir!';
+
+const doingThing = (func) => console.log(func());
+
+doingThing(wakingUp);
