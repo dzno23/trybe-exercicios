f286e9b039f94083f18d18f324127b1ec64ee117
thiagohcc <dzno23@gmail.com>
2022-10-24 17:15:10 -0300

    front-end-secao02-dia-01

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import About from './Components/About';
+
+function App() {
+  return (
+      <About />
+  );
+}
+
+export default App;
---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/Components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Fundamentos</li>
+          <li>Fornte-End</li>
+        </ul>
+      </>
+
+    )
+  }
+};
+
+export default About;
